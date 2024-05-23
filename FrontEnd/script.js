92e8d8b7d41e585d4140c57fa1b55dda8b90e229
NathCad <nathalie.cadet.tb@gmail.com>
2024-05-23 10:56:36 +0200

    Ajout barre 'mode éditer' et bouton 'modifier' html et css.     -enlever la fonction dans html.-Connexion avec page login vers page d'accueil.   -Utilisation d'un map pour la liste des catégories.-Javascript doc.

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,36 +1,72 @@
 /*const { categories } = require("../Backend/models");*/
+const user = window.sessionStorage.getItem("user")
+  ? JSON.parse(window.sessionStorage.getItem("user"))
+  : null;
+
+if (user?.token) {
+  const modifierButton = document.getElementById("modifier");
+  modifierButton.style.display = "inline-block";
+  const editionButton = document.getElementById("edition");
+  editionButton.style.display = "flex";
+  const linkLogin = document.getElementById("login");
+  linkLogin.setAttribute("href", "http://localhost:5500/FrontEnd/index.html");
+  linkLogin.textContent = "Logout";
+  linkLogin.addEventListener("click", () => {
+    localStorage.removeItem("user");
+    modifierButton.style.display = "none";
+    editionButton.style.display = "none";
+  });
+}
+
 const data = await fetch("http://localhost:5678/api/works");
 const travaux = await data.json();
-console.log(data, travaux);
-
 const nomsCategories = recupererNomsCategoriesTravaux(travaux);
-console.log(nomsCategories);
-
-afficherListeTravaux(travaux);
-
 /*Selectionner div filtre pour ajouts bouton*/
 const filtreContainer = document.querySelector(".filtres");
 
+afficherListeTravaux(travaux);
+
 /* POur les autres boutons utiliser les categories récupérées plus haut*/
-for (const nomCategorie of nomsCategories) {
+for (const [id, nomCategorie] of nomsCategories) {
   const bouton = document.createElement("button");
   bouton.textContent = nomCategorie;
-  bouton.setAttribute("id", nomCategorie);
+  bouton.setAttribute("id", getCategoryHtmlId(id));
   bouton.addEventListener("click", filtreEventHandler);
   filtreContainer.appendChild(bouton);
 }
 
 Tous.addEventListener("click", filtreEventHandler);
 
+/**
+ * Créer un id unique pour le bouton filtre plutot que 1, 2, 3, etc...
+ * @param {number} id de la catégorie
+ * @returns {string} un id html
+ */
+function getCategoryHtmlId(id) {
+  return "button-filter-category-" + id;
+}
+/**
+ * récupère un id de categorie à partir d'un id html
+ * @param {string} categoryHtmlId
+ * @returns {number} l'id de la catégorie
+ */
+function getIdFromCategoryHtmlId(categoryHtmlId) {
+  return Number(categoryHtmlId.split("button-filter-category-")[1]);
+}
+
+/**
+ * fonction de gestion de l'event click bouton filtre
+ * affiche les travaux filtrés en fonction du bouton cliqué
+ * @param {*} e
+ */
 function filtreEventHandler(e) {
-  /*recuperer l'ID du bouton e*/
-  const nomCategorie = e.target.id;
-  /*en fonction du bouton je dois créer une nouvelle liste filtrée*/
-  if (nomCategorie === "Tous") {
+  if (e.target.id === "Tous") {
     afficherListeTravaux(travaux);
   } else {
+    /*en fonction du bouton je dois créer une nouvelle liste filtrée*/
+    /*recuperer l'ID du bouton e*/
     const travauxFiltres = travaux.filter(
-      (travail) => nomCategorie === travail.category.name
+      (travail) => getIdFromCategoryHtmlId(e.target.id) === travail.category.id
     );
     /* rappeler la fonction contenu avec la liste filtrée*/
     afficherListeTravaux(travauxFiltres);
@@ -46,6 +82,11 @@ function filtreEventHandler(e) {
   }
 }
 
+/**
+ * vide la gallerie et recréé tous les elements de la galerie
+ * (figure avec image et figcaption en children)
+ * @param {*} travaux
+ */
 function afficherListeTravaux(travaux) {
   /*vider gallerie*/
   const gallery = document.querySelector(".gallery");
@@ -65,13 +106,16 @@ function afficherListeTravaux(travaux) {
     gallery.appendChild(figure);
   }
 }
-
+/**
+ * recupère dans un map les catégories présentes dans la liste des travaux
+ * @param {object[]} travaux
+ * @returns
+ */
 function recupererNomsCategoriesTravaux(travaux) {
-  const categorieTravaux = new Set();
+  const categorieTravaux = new Map();
   for (const travail of travaux) {
     /*recuperer nom des categories*/
-    console.log(travail.category.name);
-    categorieTravaux.add(travail.category.name);
+    categorieTravaux.set(travail.category.id, travail.category.name);
   }
   return categorieTravaux;
 }
--- FrontEnd/scriptLogin.js
+++ FrontEnd/scriptLogin.js
@@ -1,7 +1,16 @@
-function handleLogin() {
+document.getElementById("loginForm").addEventListener("submit", handleLogin);
+
+/**
+ * fonction de gestion du submit du formulaire login
+ * valide la saisie de l'utilisateur puis affiche une erreur ou effectue
+ * la procedure de connexion
+ * @param {*} e
+ */
+function handleLogin(e) {
+  /*empêche l'envoie du formulaire au serveveur*/
+  e.preventDefault();
   const email = document.querySelector("#email").value;
   const password = document.querySelector("#password").value;
-  console.log({ email, password });
   //Valider la saisie
   if (!email || !password) {
     showError("Veuillez remplir email et mot de passe");
@@ -9,17 +18,28 @@ function handleLogin() {
     loginToServer(email, password);
   }
 }
+
+/**
+ * Remplace le texte de l'element '.error' et le rend visible
+ * @param {string} text texte à afficher dans l'element '.error'
+ */
 function showError(text) {
   const errorElement = document.querySelector(".error");
   errorElement.textContent = text;
   errorElement.style.visibility = "visible";
 }
 
+/**
+ * procedure de connexion
+ * effectue un appel en post sur le endpoint /users/login
+ * gère le retour en affichant une erreur ou en lançant la procedure de succès du login
+ * @param {string} email
+ * @param {string} password
+ */
 async function loginToServer(email, password) {
   const response = await fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     headers: {
-      Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password }),
@@ -32,8 +52,13 @@ async function loginToServer(email, password) {
     handleUserLoggedIn(json);
   }
 }
+
+/**
+ * gestion du succès du login
+ * ajoute le user loggé au session storage et redirige vers la page d'accueil
+ * @param {object} json retour de l'API
+ */
 function handleUserLoggedIn(json) {
-  console.log(json);
-  sessionStorage.setItem("user", JSON.stringify(json));
-  window.location.href = "http://localhost:5500/FrontEnd/index.html";
+  window.sessionStorage.setItem("user", JSON.stringify(json));
+  window.location.replace("http://localhost:5500/FrontEnd/index.html");
 }
