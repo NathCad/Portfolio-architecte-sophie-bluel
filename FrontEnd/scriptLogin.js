94c449a76accbe2c71843b7c3cc5b2416c93d00a
NathCad <nathalie.cadet.tb@gmail.com>
2024-06-12 15:20:45 +0200

    Refacto script login

--- FrontEnd/scriptLogin.js
+++ FrontEnd/scriptLogin.js
@@ -1,7 +1,47 @@
-const loginApiUrl = "http://localhost:5678/api/users/login";
-const accueilUrl = "index.html";
+//=================================== Does =======================================//
+/**
+ * procedure de connexion
+ * effectue un appel en post sur le endpoint /users/login
+ * gère le retour en affichant une erreur ou en lançant la procedure de succès du login
+ * @param {string} email
+ * @param {string} password
+ */
+async function doLoginToServer(email, password) {
+  const response = await fetch(loginApiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+  console.log(response);
+  if (!response.ok) {
+    doShowError("Identifiant/mot de passe invalide");
+  } else {
+    const json = await response.json();
+    doSaveUserLogStatusAndRedirect(json);
+  }
+}
+/**
+ * gestion du succès du login
+ * ajoute le user loggé au session storage et redirige vers la page d'accueil
+ * @param {object} json retour de l'API
+ */
+function doSaveUserLogStatusAndRedirect(json) {
+  window.sessionStorage.setItem("user", JSON.stringify(json));
+  window.location.replace(redirectionUrl);
+}
 
-document.getElementById("loginForm").addEventListener("submit", handleLogin);
+/**
+ * Remplace le texte de l'element '.error' et le rend visible
+ * @param {string} text texte à afficher dans l'element '.error'
+ */
+function doShowError(text) {
+  const errorElement = document.querySelector(".error");
+  errorElement.textContent = text;
+}
+
+//=================================== Handlers =======================================//
 
 /**
  * fonction de gestion du submit du formulaire login
@@ -10,56 +50,21 @@ document.getElementById("loginForm").addEventListener("submit", handleLogin);
  * @param {*} e
  */
 function handleLogin(e) {
-  /**
-   * procedure de connexion
-   * effectue un appel en post sur le endpoint /users/login
-   * gère le retour en affichant une erreur ou en lançant la procedure de succès du login
-   * @param {string} email
-   * @param {string} password
-   */
-  async function loginToServer(email, password) {
-    const response = await fetch(loginApiUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
-    console.log(response);
-    if (!response.ok) {
-      showError("Identifiant/mot de passe invalide");
-    } else {
-      const json = await response.json();
-      handleUserLoggedIn(json);
-    }
-  }
   /*empêche l'envoie du formulaire au serveveur*/
   e.preventDefault();
   const email = document.querySelector("#email").value;
   const password = document.querySelector("#password").value;
   //Valider la saisie
   if (!email || !password) {
-    showError("Veuillez remplir email et mot de passe");
+    doShowError("Veuillez remplir email et mot de passe");
   } else {
-    loginToServer(email, password);
+    doLoginToServer(email, password);
   }
 }
 
-/**
- * Remplace le texte de l'element '.error' et le rend visible
- * @param {string} text texte à afficher dans l'element '.error'
- */
-function showError(text) {
-  const errorElement = document.querySelector(".error");
-  errorElement.textContent = text;
-}
+//=================================== Main script =======================================//
 
-/**
- * gestion du succès du login
- * ajoute le user loggé au session storage et redirige vers la page d'accueil
- * @param {object} json retour de l'API
- */
-function handleUserLoggedIn(json) {
-  window.sessionStorage.setItem("user", JSON.stringify(json));
-  window.location.replace(accueilUrl);
-}
+const loginApiUrl = "http://localhost:5678/api/users/login";
+const redirectionUrl = "index.html";
+
+document.getElementById("loginForm").addEventListener("submit", handleLogin);
